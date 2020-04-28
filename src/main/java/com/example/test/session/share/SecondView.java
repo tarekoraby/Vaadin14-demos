e5be6f5cf1b4024d8c5d5ee0fd4e67b3bd021317
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-04-28 15:43:08 +0300

    Create session sharing example

---
+++ src/main/java/com/example/test/session/share/SecondView.java
@@ -0,0 +1,43 @@
+package com.example.test.session.share;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
+import com.vaadin.flow.router.OptionalParameter;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
+
+
+/**
+ * The main view contains a button and a click listener.
+ */
+@Route("session/share/SecondView")
+public class SecondView extends VerticalLayout {
+
+	Grid<?> grid;
+	
+	public SecondView() {
+		add("GRID VIEW");
+		
+		this.grid = (Grid<?>) VaadinSession.getCurrent().getAttribute("grid");
+		if (grid==null) {
+			add("NO DATA");		
+		} else {
+			add(grid);
+		}
+	}
+	
+	void configureUndock() {					
+		add(new Button("Undock", e -> {
+			grid.getElement().removeFromTree();
+			UI.getCurrent().getPage().executeJs("window.open('grid')");
+		}));		
+	}
+	
+		
+}
