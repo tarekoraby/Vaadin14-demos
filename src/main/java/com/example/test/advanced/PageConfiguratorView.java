0364101194bdf4b18edbe8be468bf9fea027dc6c
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-06-04 10:52:09 +0300

    Update PageConfiguratorView.java

--- src/main/java/com/example/test/advanced/PageConfiguratorView.java
+++ src/main/java/com/example/test/advanced/PageConfiguratorView.java
@@ -1,12 +1,6 @@
 package com.example.test.advanced;
 
-import com.example.test.GreetService;
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.InitialPageSettings;
 import com.vaadin.flow.server.PageConfigurator;
