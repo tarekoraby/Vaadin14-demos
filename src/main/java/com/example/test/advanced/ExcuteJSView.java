63b489ad77b96b534beab002bd6bfabb6aaa94ca
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-06-05 11:53:41 +0300

    Create ExcuteJSView.java

---
+++ src/main/java/com/example/test/advanced/ExcuteJSView.java
@@ -0,0 +1,22 @@
+package com.example.test.advanced;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("ExcuteJSView")
+public class ExcuteJSView extends VerticalLayout {
+
+
+    public ExcuteJSView() {
+        Button button = new Button("Hide me!!");
+
+        button.addClickListener(event -> {
+            button.getElement()
+                    .executeJs(
+                            "this.style.display = \"none\"");
+        });
+
+        add(button);
+    }
+}
