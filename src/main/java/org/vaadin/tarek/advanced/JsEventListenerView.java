69c491afd80b94aa6560e02abc584b26d089958a
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-06-24 09:46:41 +0300

    Create JsEventListenerView.java

---
+++ src/main/java/org/vaadin/tarek/advanced/JsEventListenerView.java
@@ -0,0 +1,45 @@
+package org.vaadin.tarek.advanced;
+
+import com.vaadin.flow.component.ClientCallable;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+@Route("JsEventListenerView")
+public class JsEventListenerView extends VerticalLayout {
+
+    Paragraph p;
+
+    public JsEventListenerView() {
+        TextField tf = new TextField("Label");
+
+        tf.getElement().executeJs(
+                "this.addEventListener(\"mouseover\", () => $0.$server.textFieldMouseOver())",
+                getElement());
+        tf.getElement().executeJs(
+                "this.addEventListener(\"click\", () => $0.$server.textFieldClicked())",
+                getElement());
+        tf.getElement().executeJs(
+                "this.addEventListener(\"mouseout\", () => $0.$server.textFieldMouseOut())",
+                getElement());
+
+        p = new Paragraph("Initial element status");
+        add(tf, p);
+    }
+
+    @ClientCallable
+    public void textFieldMouseOver() {
+        p.setText("Mouse over element");
+    }
+
+    @ClientCallable
+    public void textFieldClicked() {
+        p.setText("Element clicked");
+    }
+
+    @ClientCallable
+    public void textFieldMouseOut() {
+        p.setText("Mouse out of element");
+    }
+}
