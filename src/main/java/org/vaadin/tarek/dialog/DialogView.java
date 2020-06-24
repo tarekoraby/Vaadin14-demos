a0e5c57b53bc2fbe6c2f22406cc26079dfe80ff0
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-06-24 09:27:49 +0300

    Create DialogView.java

---
+++ src/main/java/org/vaadin/tarek/dialog/DialogView.java
@@ -0,0 +1,25 @@
+package org.vaadin.tarek.dialog;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("DialogView")
+public class DialogView extends VerticalLayout {
+
+    public DialogView() {
+        Button button = new Button("Open Dialog");
+        Dialog dialog = new Dialog();
+        dialog.add(new Label("Close me with the esc-key or an outside click"));
+
+        dialog.setWidth("400px");
+        dialog.setHeight("150px");
+
+        button.addClickListener(event -> dialog.open());
+
+        add(button);
+    }
+
+}
