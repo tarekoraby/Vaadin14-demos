6ee62d3848ab9a98e13cdc3592e00f970ea0c25f
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-07-28 08:44:49 +0300

    Add Dialog's overlay styling

--- src/main/java/org/vaadin/tarek/dialog/DialogView.java
+++ src/main/java/org/vaadin/tarek/dialog/DialogView.java
@@ -1,12 +1,14 @@
 package org.vaadin.tarek.dialog;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("DialogView")
+@CssImport(value = "./styles/vaadin-dialog-overlay.css", themeFor = "vaadin-dialog-overlay")
 public class DialogView extends VerticalLayout {
 
     public DialogView() {
