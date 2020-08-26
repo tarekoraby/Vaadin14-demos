83d3a94402942e85b3211f392b2895956a4bcaf7
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-08-26 10:33:57 +0300

    Update AppLayoutSimpleView

--- src/main/java/org/vaadin/tarek/applayout/AppLayoutSimpleView.java
+++ src/main/java/org/vaadin/tarek/applayout/AppLayoutSimpleView.java
@@ -2,12 +2,15 @@ package org.vaadin.tarek.applayout;
 
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
 
 @Route("AppLayoutSimpleView")
+@CssImport(value = "./styles/vaadin-app-layout-styles.css", themeFor = "vaadin-app-layout")
 public class AppLayoutSimpleView extends AppLayout {
 
     public AppLayoutSimpleView() {
@@ -18,7 +21,8 @@ public class AppLayoutSimpleView extends AppLayout {
         tabs.add(new Tab(new RouterLink("tab 3", AppLayoutSimpleView.class)));
         addToDrawer(tabs);
 
-        addToNavbar(new Button("navbar 1"));
+        addToNavbar(new Anchor("https://vaadin.com", "vaadin"),
+                new Anchor("https://google.com", "google"));
 
         setContent(new Button("content"));
     }
