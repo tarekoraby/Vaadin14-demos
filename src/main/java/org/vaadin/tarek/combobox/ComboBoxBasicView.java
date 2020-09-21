819160da83f48ba457ad10bd01f38913fbc53555
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-09-21 10:08:54 +0300

    Update combobox styles

--- src/main/java/org/vaadin/tarek/combobox/ComboBoxBasicView.java
+++ src/main/java/org/vaadin/tarek/combobox/ComboBoxBasicView.java
@@ -6,7 +6,9 @@ import java.util.List;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.combobox.ComboBox.ItemFilter;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 
 /**
@@ -18,23 +20,21 @@ import com.vaadin.flow.router.Route;
 public class ComboBoxBasicView extends VerticalLayout {
 
     public ComboBoxBasicView() {
-        ComboBox<String> simpleComboBox = new ComboBox<>();
-        simpleComboBox.setItems("Option one", "Option two", "Option three");
-        simpleComboBox.setValue("Option one");
-        simpleComboBox.setLabel("Simple ComboBox");
+        ComboBox<String> simple = new ComboBox<>();
+        simple.setItems("Option one", "Option two", "Option three");
+        simple.setValue("Option one");
+        simple.setLabel("Simple ComboBox");
 
-        ComboBox<String> wideOverlayComboBox = new ComboBox<>();
-        wideOverlayComboBox.setItems("Option one", "Option two");
-        wideOverlayComboBox.setPlaceholder("Placeholder");
-        wideOverlayComboBox.getElement().setAttribute("theme",
-                "wide-combobox-overlay");
-        wideOverlayComboBox.setLabel("Wide-Overlay ComboBox");
+        ComboBox<String> wideOverlay = new ComboBox<>();
+        wideOverlay.setItems("Option one", "Option two");
+        wideOverlay.setPlaceholder("Placeholder");
+        wideOverlay.getElement().setAttribute("theme", "wide-combobox-overlay");
+        wideOverlay.setLabel("Wide-Overlay ComboBox");
 
-        ComboBox<String> condensedListComboBox = new ComboBox<>();
-        condensedListComboBox.setItems("Option one", "Option two");
-        condensedListComboBox.getElement().setAttribute("theme",
-                "condensed-list-combobox");
-        condensedListComboBox.setLabel("Condensed-List ComboBox");
+        ComboBox<String> condensedList = new ComboBox<>();
+        condensedList.setItems("Option one", "Option two");
+        condensedList.getElement().setAttribute("theme", "custom-spacing");
+        condensedList.setLabel("Condensed-List ComboBox");
 
         ComboBox<String> filteringComboBox = new ComboBox<>();
         List<String> elementsList = new ArrayList<>();
@@ -57,7 +57,54 @@ public class ComboBoxBasicView extends VerticalLayout {
         filteringComboBox.setClearButtonVisible(true);
         filteringComboBox.setLabel("Filtering ComboBox");
 
-        add(simpleComboBox, wideOverlayComboBox, condensedListComboBox,
-                filteringComboBox);
+        ComboBox<String> customItemColor = new ComboBox<>();
+        customItemColor.setItems("Good #1", "Bad #1", "Good #2", "Good #3",
+                "Bad #2");
+        customItemColor.setRenderer(new ComponentRenderer<>(str -> {
+            Span text = new Span(str);
+            if (str.startsWith("Bad")) {
+                text.getStyle().set("color", "red");
+            } else if (str.startsWith("Good")) {
+                text.getStyle().set("color", "green");
+            }
+            return text;
+        }));
+        customItemColor.setLabel("Custom item color");
+
+        ComboBox<String> customBackground = new ComboBox<>();
+        customBackground.setItems("Good #1", "Bad #1", "Good #2", "Good #3",
+                "Bad #2");
+        customBackground.setRenderer(new ComponentRenderer<>(str -> {
+            Span text = new Span(str);
+            if (str.startsWith("Bad")) {
+                text.setClassName("bad");
+            } else if (str.startsWith("Good")) {
+                text.setClassName("good");
+            }
+            return text;
+        }));
+        customBackground.getElement().setAttribute("theme",
+                "custom-item-background");
+        customBackground.setLabel("Custom item background");
+        customBackground.setValue("Good #2");
+
+        ComboBox<String> hiddenSelectionIcon = new ComboBox<>();
+        hiddenSelectionIcon.setItems("Good #1", "Bad #1", "Good #2", "Good #3",
+                "Bad #2");
+        hiddenSelectionIcon.getElement().setAttribute("theme",
+                "hide-selection-button");
+        hiddenSelectionIcon.setLabel("Hidden selection icon");
+
+        ComboBox<String> customSelectionIcons = new ComboBox<>();
+        customSelectionIcons.setItems("Option one", "Option two",
+                "Option three");
+        customSelectionIcons.setValue("Option one");
+        customSelectionIcons.getElement().setAttribute("theme",
+                "custom-selection-icons");
+        customSelectionIcons.setLabel("Custom selection icons");
+
+        add(simple, wideOverlay, condensedList, filteringComboBox,
+                customItemColor, customBackground, hiddenSelectionIcon,
+                customSelectionIcons);
     }
 }
