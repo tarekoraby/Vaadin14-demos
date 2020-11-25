bac94da9b1f2c8cba7bcfde45948b76145535843
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-11-25 10:22:14 +0200

    Update ConfirmDialogView.java

--- src/main/java/org/vaadin/tarek/confirmdialog/ConfirmDialogView.java
+++ src/main/java/org/vaadin/tarek/confirmdialog/ConfirmDialogView.java
@@ -1,5 +1,6 @@
 package org.vaadin.tarek.confirmdialog;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.dependency.CssImport;
@@ -12,12 +13,30 @@ import com.vaadin.flow.router.Route;
 public class ConfirmDialogView extends VerticalLayout {
 
     public ConfirmDialogView() {
-        ConfirmDialog dialog = new ConfirmDialog("Confirm publish",
+        ConfirmDialog simpleDialog = createDialog();
+        Button buttonA = new Button("Open simple dialog",
+                event -> simpleDialog.open());
+
+        ConfirmDialog styledMessageDialog = createDialog();
+        String themeName = "myConfirmDialog";
+        styledMessageDialog.getElement().setAttribute("theme", themeName);
+        Button buttonB = new Button("Open dialog wih styled message", e -> {
+            styledMessageDialog.open();
+            String color = "green";
+            UI.getCurrent().getPage().executeJs(
+                    "document.querySelector(\"[theme='_vaadin-confirm-dialog-dialog-overlay-theme "
+                            + themeName
+                            + "']\").$.content.shadowRoot.querySelector(\"[part='message']\").style.setProperty('color', '"
+                            + color + "')");
+        });
+
+        add(buttonA, buttonB);
+    }
+
+    private ConfirmDialog createDialog() {
+        return new ConfirmDialog("Confirm publish",
                 "Are you sure you want to publish the article?", "Publish",
                 e -> onPublish(), "Cancel", c -> onCancel());
-        Button button = new Button("Open dialog");
-        button.addClickListener(event -> dialog.open());
-        add(button, dialog);
     }
 
     private void onCancel() {
