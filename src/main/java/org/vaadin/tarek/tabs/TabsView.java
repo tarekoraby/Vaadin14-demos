2da2037269a99207a3d9fa9577ca0a3dc9ba57fc
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-01-04 10:34:40 +0200

    Add TabsView

---
+++ src/main/java/org/vaadin/tarek/tabs/TabsView.java
@@ -0,0 +1,41 @@
+package org.vaadin.tarek.tabs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.router.Route;
+
+@Route("TabsView")
+public class TabsView extends VerticalLayout {
+
+    public TabsView() {
+        Tab tab1 = new Tab("Tab one");
+        Div page1 = new Div();
+        page1.setText("Page#1");
+
+        Tab tab2 = new Tab("Tab two");
+        Div page2 = new Div();
+        page2.setText("Page#2");
+        page2.setVisible(false);
+
+        Map<Tab, Component> tabsToPages = new HashMap<>();
+        tabsToPages.put(tab1, page1);
+        tabsToPages.put(tab2, page2);
+        Tabs tabs = new Tabs(tab1, tab2);
+        Div pages = new Div(page1, page2);
+        pages.setSizeFull();
+
+        tabs.addSelectedChangeListener(event -> {
+            tabsToPages.values().forEach(page -> page.setVisible(false));
+            Component selectedPage = tabsToPages.get(tabs.getSelectedTab());
+            selectedPage.setVisible(true);
+        });
+
+        add(tabs, pages);
+    }
+}
