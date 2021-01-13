7b1502c3da02a3f50624269c92154f80904c3d18
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-01-13 09:37:02 +0200

    Update SelectView stylings

--- src/main/java/org/vaadin/tarek/select/SelectView.java
+++ src/main/java/org/vaadin/tarek/select/SelectView.java
@@ -7,16 +7,19 @@ import com.vaadin.flow.router.Route;
 
 @Route("SelectView")
 @CssImport(value = "./styles/vaadin-select-text-field-styles.css", themeFor = "vaadin-select-text-field")
+@CssImport(value = "./styles/vaadin-select-styles.css", themeFor = "vaadin-select")
 public class SelectView extends VerticalLayout {
 
     public SelectView() {
         Select<String> styledSelect = new Select<>();
         styledSelect.setItems("Option one", "Option two");
         styledSelect.setLabel("Styled Select");
-        styledSelect.getElement().setAttribute("theme", "my-styled-select");
+        styledSelect.setPlaceholder("Placeholder");
+        styledSelect.addClassName("my-styled-select");
 
         Select<String> nonStyledSelect = new Select<>();
         nonStyledSelect.setItems("Option one", "Option two");
+        nonStyledSelect.setPlaceholder("Placeholder");
         nonStyledSelect.setLabel("Non-styled Select");
 
         add(styledSelect, nonStyledSelect);
