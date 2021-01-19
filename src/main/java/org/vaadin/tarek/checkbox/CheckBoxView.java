5687350ef02fd8c4b599d1ecf3f791b2dcfbe274
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-01-19 13:23:53 +0200

    Add swapped CheckBox

--- src/main/java/org/vaadin/tarek/checkbox/CheckBoxView.java
+++ src/main/java/org/vaadin/tarek/checkbox/CheckBoxView.java
@@ -2,11 +2,14 @@ package org.vaadin.tarek.checkbox;
 
 import java.util.Collections;
 
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.checkbox.CheckboxGroup;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("CheckBoxView")
+@CssImport(value = "./styles/vaadin-checkbox-styles.css", themeFor = "vaadin-checkbox")
 public class CheckBoxView extends VerticalLayout {
 
     public CheckBoxView() {
@@ -15,6 +18,9 @@ public class CheckBoxView extends VerticalLayout {
         checkboxGroup.setItems("Option one", "Option two");
         checkboxGroup.setValue(Collections.singleton("Option one"));
 
-        add(checkboxGroup);
+        Checkbox checkboxSwapped = new Checkbox("Swapped label");
+        checkboxSwapped.addClassName("swapped");
+
+        add(checkboxGroup, checkboxSwapped);
     }
 }
