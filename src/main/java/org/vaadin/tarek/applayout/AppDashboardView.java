6456365ad3d1182fdbcbd4636cf5897d2a24a090
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-02-19 10:28:34 +0200

    Add scroll to top button

--- src/main/java/org/vaadin/tarek/applayout/AppDashboardView.java
+++ src/main/java/org/vaadin/tarek/applayout/AppDashboardView.java
@@ -1,5 +1,6 @@
 package org.vaadin.tarek.applayout;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -11,5 +12,9 @@ public class AppDashboardView extends VerticalLayout {
         for (int i = 0; i < 50; i++) {
             add(new Span("Admin view content"));
         }
+
+        add(new Button("scroll to top",
+                e -> getElement().executeJs(
+                        "document.querySelector('vaadin-app-layout').shadowRoot.querySelector('[content]').scrollTo(0, 0)")));
     }
 }
