9a0646c6d544bcde3e99b39164628b61c2f9e7e0
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-03-12 15:51:59 +0200

    Add Checkbox-es and their event handlers

--- frontend/src/hello-polymer.js
+++ frontend/src/hello-polymer.js
@@ -1,21 +1,41 @@
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 
 class HelloPolymer extends PolymerElement {
-	static get template() {
-		return html`
-		<h2>Hello World!</h2>
-		<div id="content"></div>
-		<slot></slot>
-		<hr>
-		<button on-click="handleClick">Button with server- and client-side event handlers</button>`;
-		}
-	
-	handleClick() {
-		console.log('Button was clicked !!!!');
-		}
-	
-	static get is() {
-		return 'hello-polymer';
-		}
-	}
+  static get properties() {
+    return {
+      /** @private */
+      _checkboxGroupElement: Object,
+    };
+  }
+
+  static get template() {
+    return html` <h2>Hello World!</h2>
+      <div id="content"></div>
+      <slot></slot>
+      <hr />
+      <button on-click="handleClick">Button with server- and client-side event handlers</button>
+      <br />
+      <vaadin-checkbox-group on-change="handleChange" id="proxy-checkbox" label="" theme="vertical">
+        <vaadin-checkbox>English</vaadin-checkbox>
+        <vaadin-checkbox>French</vaadin-checkbox>
+      </vaadin-checkbox-group>`;
+  }
+
+  handleClick() {
+    console.log('Button was clicked !!!!');
+  }
+
+  /** @protected */
+  ready() {
+    super.ready();
+
+    this._checkboxGroupElement = this.shadowRoot.querySelector('vaadin-checkbox-group');
+    this._checkboxGroupElement.childNodes[0].value = 'En';
+    this._checkboxGroupElement.childNodes[1].value = 'Fr';
+  }
+
+  static get is() {
+    return 'hello-polymer';
+  }
+}
 customElements.define(HelloPolymer.is, HelloPolymer);
--- src/main/java/org/vaadin/tarek/components/custom/HelloPolymer.java
+++ src/main/java/org/vaadin/tarek/components/custom/HelloPolymer.java
@@ -1,9 +1,11 @@
 package org.vaadin.tarek.components.custom;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.EventData;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.polymertemplate.EventHandler;
 import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
@@ -17,12 +19,18 @@ public class HelloPolymer extends PolymerTemplate<TemplateModel> {
     private Div content;
 
     public void setContent(Component component) {
-        this.content.removeAll();
-        this.content.add(component);
+        content.removeAll();
+        content.add(component);
     }
 
     @EventHandler
     private void handleClick() {
         System.out.println("Received a click event");
+    }
+
+    @EventHandler
+    private void handleChange(@EventData("event.srcElement.value") String value,
+            @EventData("event.srcElement.checked") Boolean checked) {
+        Notification.show("Change event received: " + value + ", isChecked: " + checked);
     }
 }
